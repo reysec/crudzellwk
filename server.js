94bbdb9053a885b67d1b87a93619c734f352114a
Angga Pramudya <pramudya.angga58@gmail.com>
2018-02-21 16:00:10 +0700

    Adding POST to MLAB

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+
+var db
+
+MongoClient.connect('mongodb://aang58:P4ssw0rd@ds245228.mlab.com:45228/crudzellwk_db', (err, database) => {
+    //... start the server
+    if (err) return console.log(err);
+    db = database
+    app.listen(process.env.PORT || 3000, () => {
+        console.log('listening on 3000');
+    })
+})
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, function() {
-    console.log('listening on 3000');
-})
+// app.listen(3000, function() {
+//     console.log('listening on 3000');
+// })
 
 // app.get('/', function(req, res) {
 //     res.send('Hello World');
@@ -19,7 +31,12 @@ app.get('/', (req,res) => {
 })
 
 app.post('/quotes', (req, res) => {
-    console.log(req.body);
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('saved to database')
+        res.redirect('/')
+    })
 })
 
 console.log("Hallo ini nodejs - javascript didalam server");
